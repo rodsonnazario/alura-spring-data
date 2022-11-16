3004bb269447d85d2927f8eb52efc3472378dae7
rodsonnazario <rodson.nazario@gmail.com>
2022-11-16 00:01:00 -0300

    criando novas entidades, repository e services

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -7,15 +7,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.alura.spring.data.service.CrudCargoService;
+import br.com.alura.spring.data.service.CrudFuncionarioService;
+import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
 
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
 
 	private final CrudCargoService cargoService;
+	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
+	private final CrudFuncionarioService funcionarioService;
+
 	private Boolean system = true;
 
-	public SpringDataApplication(CrudCargoService cargoService) {
+	public SpringDataApplication(CrudCargoService cargoService, CrudUnidadeTrabalhoService unidadeTrabalhoService,
+			CrudFuncionarioService funcionarioService) {
 		this.cargoService = cargoService;
+		this.unidadeTrabalhoService = unidadeTrabalhoService;
+		this.funcionarioService = funcionarioService;
 	}
 
 	public static void main(String[] args) {
@@ -29,15 +37,27 @@ public class SpringDataApplication implements CommandLineRunner {
 		while (system) {
 			System.out.println("Qual acao voce quer executar?");
 			System.out.println("0 - Sair");
-			System.out.println("1 - Cargo");
+			System.out.println("1 - Funcionario");
+			System.out.println("2 - Cargo");
+			System.out.println("3 - Unidade");
 
-			int action = scanner.nextInt();
-			if (action == 1) {
+			Integer action = scanner.nextInt();
+
+			switch (action) {
+			case 1:
+				funcionarioService.inicial(scanner);
+				break;
+			case 2:
 				cargoService.inicial(scanner);
-			} else {
+				break;
+			case 3:
+				unidadeTrabalhoService.inicial(scanner);
+				break;
+			default:
+				System.out.println("Finalizando");
 				system = false;
+				break;
 			}
 		}
 	}
-
 }
--- src/main/java/br/com/alura/spring/data/orm/UnidadeTrabalho.java
+++ src/main/java/br/com/alura/spring/data/orm/UnidadeTrabalho.java
@@ -3,22 +3,24 @@ package br.com.alura.spring.data.orm;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "cargos")
-public class Cargo {
+@Table(name = "unidade_trabalho")
+public class UnidadeTrabalho {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String descricao;
-	@OneToMany(mappedBy = "cargo")
-	private List<Funcionario> funcionarios;
+	private String endereco;
+	@ManyToMany(mappedBy = "unidadeTrabalhos", fetch = FetchType.EAGER)
+	private List<Funcionario> funcionario;
 
 	public Integer getId() {
 		return id;
@@ -36,8 +38,19 @@ public class Cargo {
 		this.descricao = descricao;
 	}
 
-	@Override
-	public String toString() {
-		return "Cargo [id=" + id + ", descricao=" + descricao + "]";
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public List<Funcionario> getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(List<Funcionario> funcionario) {
+		this.funcionario = funcionario;
 	}
 }
